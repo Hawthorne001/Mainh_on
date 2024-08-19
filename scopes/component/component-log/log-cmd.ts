e173a6fabe2fe73253e087a462f7021c03b00bd6
Tyler Hawthorne <110597351+Hawthorne001@users.noreply.github.com>
2024-08-19 01:29:07 -0400

    Initial commit

---
+++ scopes/component/component-log/log-cmd.ts
@@ -0,0 +1,121 @@
+import c from 'chalk';
+import Table from 'cli-table';
+import { Command, CommandOptions } from '@teambit/cli';
+import { LegacyComponentLog } from '@teambit/legacy-component-log';
+import { ComponentLogMain } from './component-log.main.runtime';
+
+export default class LogCmd implements Command {
+  name = 'log <id>';
+  description = 'show components(s) version history';
+  helpUrl = 'reference/components/navigating-history';
+  extendedDescription: string;
+  group = 'info';
+  alias = '';
+  options = [
+    ['r', 'remote', 'show log of a remote component'],
+    ['', 'parents', 'show parents and lanes data'],
+    ['o', 'one-line', 'show each log entry in one line'],
+    ['f', 'full-hash', 'show full hash of the snap (default to the first 9 characters for --one-line/--parents flags)'],
+    ['m', 'full-message', 'show full message of the snap (default to the first line for --one-line/--parents flags)'],
+    ['j', 'json', 'json format'],
+  ] as CommandOptions;
+  remoteOp = true; // should support log against remote
+  skipWorkspace = true;
+  arguments = [{ name: 'id', description: 'component-id or component-name' }];
+
+  constructor(private componentLog: ComponentLogMain) {}
+
+  async report(
+    [id]: [string],
+    {
+      remote = false,
+      parents = false,
+      oneLine = false,
+      fullHash = false,
+      fullMessage,
+    }: { remote: boolean; parents: boolean; oneLine?: boolean; fullHash?: boolean; fullMessage?: boolean }
+  ) {
+    if (!parents && !oneLine) {
+      fullHash = true;
+      fullMessage = true;
+    }
+    if (parents) {
+      const logs = await this.componentLog.getLogsWithParents(id, fullHash, fullMessage);
+      return logs.join('\n');
+    }
+    const logs = await this.componentLog.getLogs(id, remote, !fullHash, !fullMessage);
+    if (oneLine) {
+      return logOneLine(logs.reverse());
+    }
+    // reverse to show from the latest to earliest
+    return logs.reverse().map(paintLog).join('\n');
+  }
+
+  async json([id]: [string], { remote = false, parents = false }: { remote: boolean; parents: boolean }) {
+    if (parents) {
+      return this.componentLog.getLogsWithParents(id);
+    }
+    return this.componentLog.getLogs(id, remote);
+  }
+}
+
+export function paintAuthor(email: string | null | undefined, username: string | null | undefined) {
+  if (email && username) {
+    return c.white(`author: ${username} <${email}>\n`);
+  }
+  if (email && !username) {
+    return c.white(`author: <${email}>\n`);
+  }
+  if (!email && username) {
+    return c.white(`author: ${username}\n`);
+  }
+
+  return '';
+}
+
+function paintLog(log: LegacyComponentLog): string {
+  const { message, date, tag, hash, username, email } = log;
+  const title = tag ? `tag ${tag} (${hash})\n` : `snap ${hash}\n`;
+  return (
+    c.yellow(title) +
+    paintAuthor(email, username) +
+    (date ? c.white(`date: ${date}\n`) : '') +
+    (message ? c.white(`\n      ${message}\n`) : '')
+  );
+}
+
+/**
+ * table with no style and no borders, just to align the columns.
+ */
+export function getEmptyTableWithoutStyle() {
+  return new Table({
+    chars: {
+      top: '',
+      'top-mid': '',
+      'top-left': '',
+      'top-right': '',
+      bottom: '',
+      'bottom-mid': '',
+      'bottom-left': '',
+      'bottom-right': '',
+      left: '',
+      'left-mid': '',
+      mid: '',
+      'mid-mid': '',
+      right: '',
+      'right-mid': '',
+      middle: ' ',
+    },
+    style: { 'padding-left': 0, 'padding-right': 0 },
+  });
+}
+
+function logOneLine(logs: LegacyComponentLog[]) {
+  const table = getEmptyTableWithoutStyle();
+
+  logs.map(({ hash, tag, username, date, message }) =>
+    table.push([hash, tag || '', username || '', date || '', message || ''])
+  );
+
+  return table.toString();
+}
---
+++ scopes/component/dev-files/dev-files.main.runtime.ts
@@ -0,0 +1,254 @@
+import { SourceFile } from '@teambit/legacy/dist/consumer/component/sources';
+import { MainRuntime } from '@teambit/cli';
+import { parse } from 'comment-json';
+import { ScopeAspect, ScopeMain } from '@teambit/scope';
+import { flatten, isFunction } from 'lodash';
+import { SlotRegistry, Slot } from '@teambit/harmony';
+import { WorkspaceAspect, Workspace } from '@teambit/workspace';
+import { EnvsAspect, EnvsMain } from '@teambit/envs';
+import LegacyComponent from '@teambit/legacy/dist/consumer/component';
+import { Component, ComponentMain, ComponentAspect } from '@teambit/component';
+import { GraphqlAspect, GraphqlMain } from '@teambit/graphql';
+import { DevFilesAspect } from './dev-files.aspect';
+import { DevFiles } from './dev-files';
+import { DevFilesFragment } from './dev-files.fragment';
+import { devFilesSchema } from './dev-files.graphql';
+
+type DevPatternDescriptor = {
+  /**
+   * Name of the dev pattern
+   */
+  name: string;
+
+  /**
+   * Glob pattern to select dev files
+   */
+  pattern: string[];
+};
+
+type DevPattern = string[] | DevPatternDescriptor;
+
+/**
+ * dev pattern is a list of strings or a function that returns a list of strings. an example to a pattern can be "[*.spec.ts]"
+ */
+export type DevPatterns = ((component: Component) => DevPattern) | DevPattern;
+
+/**
+ * slot for dev file patterns.
+ */
+export type DevPatternSlot = SlotRegistry<DevPatterns>;
+
+export type DevFilesConfig = {
+  patterns: string[];
+};
+
+export class DevFilesMain {
+  constructor(
+    private envs: EnvsMain,
+
+    private workspace: Workspace,
+
+    private devPatternSlot: DevPatternSlot,
+
+    /**
+     * configuration loaded into the dev files aspect.
+     */
+    readonly config: DevFilesConfig
+  ) {}
+
+  /**
+   * compute all dev patterns on a component.
+   * computing of dev patterns is a merge of the configuration, the env (env.getDevPatterns(component)) and
+   * the registering aspects (through registerDevPattern()).
+   */
+  async computeDevPatterns(component: Component) {
+    const entry = component.state.aspects.get(DevFilesAspect.id);
+    const configuredPatterns = entry?.config.devFilePatterns || [];
+
+    const fromSlot = await this.computeDevPatternsFromSlot(component);
+    const fromEnv = await this.computeDevPatternsFromEnv(component, fromSlot.names);
+
+    const res = Object.assign(
+      {
+        config: configuredPatterns,
+      },
+      fromSlot.patterns,
+      fromEnv
+    );
+    return res;
+  }
+
+  private async computeDevPatternsFromSlot(
+    component: Component
+  ): Promise<{ patterns: { [id: string]: string[] }; names: { [name: string]: string } }> {
+    const patternSlot = this.devPatternSlot.toArray();
+    const getPatterns = (devPatterns: DevPatterns) => {
+      if (isFunction(devPatterns)) {
+        return devPatterns(component);
+      }
+      return devPatterns;
+    };
+    const fromSlot = patternSlot.reduce(
+      (acc, current) => {
+        const [aspectId, patterns] = current;
+        // if (component.state.aspects.get(aspectId)) acc[aspectId] = acc[aspectId].concat(patterns);
+        const patternsOrDescriptor = getPatterns(patterns);
+        const patternsArray = Array.isArray(patternsOrDescriptor) ? patternsOrDescriptor : patternsOrDescriptor.pattern;
+        const name = Array.isArray(patternsOrDescriptor) ? undefined : patternsOrDescriptor.name;
+        if (!acc.patterns[aspectId]) acc.patterns[aspectId] = [];
+        acc.patterns[aspectId] = acc.patterns[aspectId].concat(patternsArray);
+        if (name) {
+          acc.names[name] = aspectId;
+        }
+        return acc;
+      },
+      { patterns: {}, names: {} }
+    );
+    return fromSlot;
+  }
+
+  private async computeDevPatternsFromEnv(
+    component: Component,
+    patternNames: { [name: string]: string }
+  ): Promise<{ [id: string]: string[] }> {
+    const envId = this.envs.getEnvId(component);
+    const fromEnvJsonFile = await this.computeDevPatternsFromEnvJsoncFile(envId);
+    let fromEnvFunc;
+    if (!fromEnvJsonFile) {
+      const envDef = await this.envs.calculateEnv(component, { skipWarnings: !!this.workspace?.inInstallContext });
+      fromEnvFunc = envDef.env?.getDevPatterns ? envDef.env.getDevPatterns(component) : [];
+    }
+    const envPatterns = fromEnvJsonFile || fromEnvFunc || {};
+    if (Array.isArray(envPatterns)) {
+      return { [envId]: envPatterns };
+    }
+    const envPatternsObject = Object.entries(envPatterns).reduce((acc, [name, pattern]) => {
+      const aspectId = patternNames[name] || envId;
+      if (!acc[aspectId]) acc[aspectId] = [];
+      acc[aspectId] = acc[aspectId].concat(pattern);
+      return acc;
+    }, {});
+    return envPatternsObject;
+  }
+
+  private async computeDevPatternsFromEnvJsoncFile(
+    envId: string,
+    legacyFiles?: SourceFile[]
+  ): Promise<string[] | undefined> {
+    const isCoreEnv = this.envs.isCoreEnv(envId);
+    if (isCoreEnv) return undefined;
+    let envJson;
+    if (legacyFiles) {
+      envJson = legacyFiles.find((file) => {
+        return file.relative === 'env.jsonc' || file.relative === 'env.json';
+      });
+    } else {
+      const envComponent = await this.envs.getEnvComponentByEnvId(envId, envId);
+      envJson = envComponent.filesystem.files.find((file) => {
+        return file.relative === 'env.jsonc' || file.relative === 'env.json';
+      });
+    }
+
+    if (!envJson) return undefined;
+
+    const object = parse(envJson.contents.toString('utf8'));
+    return object.patterns;
+  }
+
+  /**
+   * get all dev files configured on a component.
+   */
+  getDevPatterns(component: Component, aspectId?: string): string[] {
+    const entry = component.state.aspects.get(DevFilesAspect.id);
+    const devPatterns = entry?.data.devPatterns || {};
+    return aspectId ? devPatterns[aspectId] : flatten(Object.values(devPatterns));
+  }
+
+  /**
+   * determine whether a file of a component is a dev file.
+   */
+  isDevFile(component: Component, filePath: string): boolean {
+    const devFiles = this.getDevFiles(component);
+    return devFiles.includes(filePath);
+  }
+
+  /**
+   * register a new dev pattern.
+   * @param regex dev pattern
+   */
+  registerDevPattern(pattern: DevPatterns) {
+    return this.devPatternSlot.register(pattern);
+  }
+
+  /**
+   * get all dev patterns registered.
+   * If you want to use this during onLoad event you might need to use computeDevFiles instead, since the component might not include this data yet
+   */
+  getDevFiles(component: Component): DevFiles {
+    const entry = component.state.aspects.get(DevFilesAspect.id);
+    const rawDevFiles = entry?.data.devFiles || {};
+    return new DevFiles(rawDevFiles);
+  }
+
+  async getDevFilesForConsumerComp(consumerComponent: LegacyComponent): Promise<string[]> {
+    const componentId = consumerComponent.id;
+    // Do not change the storeInCache=false arg. if you think you need to change it, please talk to Gilad first
+    const component = await this.workspace.get(componentId, consumerComponent, true, false, { loadExtensions: false });
+    if (!component) throw Error(`failed to transform component ${consumerComponent.id.toString()} in harmony`);
+    const computedDevFiles = await this.computeDevFiles(component);
+    return computedDevFiles.list();
+  }
+
+  /**
+   * compute all dev files of a component.
+   */
+  async computeDevFiles(component: Component): Promise<DevFiles> {
+    const devPatterns = await this.computeDevPatterns(component);
+    const rawDevFiles = Object.keys(devPatterns).reduce((acc, aspectId) => {
+      if (!acc[aspectId]) acc[aspectId] = [];
+      const patterns = devPatterns[aspectId];
+      acc[aspectId] = component.state.filesystem.byGlob(patterns).map((file) => file.relative);
+      return acc;
+    }, {});
+
+    return new DevFiles(rawDevFiles);
+  }
+
+  static slots = [Slot.withType<DevPatterns>()];
+
+  static defaultConfig = {
+    patterns: [],
+  };
+
+  static runtime = MainRuntime;
+
+  static dependencies = [EnvsAspect, WorkspaceAspect, ComponentAspect, GraphqlAspect, ScopeAspect];
+
+  static async provider(
+    [envs, workspace, componentAspect, graphql, scope]: [EnvsMain, Workspace, ComponentMain, GraphqlMain, ScopeMain],
+    config: DevFilesConfig,
+    [devPatternSlot]: [DevPatternSlot]
+  ) {
+    const devFiles = new DevFilesMain(envs, workspace, devPatternSlot, config);
+    componentAspect.registerShowFragments([new DevFilesFragment(devFiles)]);
+
+    const calcDevOnLoad = async (component: Component) => {
+      return {
+        devPatterns: await devFiles.computeDevPatterns(component),
+        devFiles: (await devFiles.computeDevFiles(component)).toObject(),
+      };
+    };
+
+    if (workspace) {
+      workspace.registerOnComponentLoad(calcDevOnLoad);
+    }
+    if (scope) {
+      scope.registerOnCompAspectReCalc(calcDevOnLoad);
+    }
+
+    graphql.register(devFilesSchema(devFiles));
+    return devFiles;
+  }
+}
+
+DevFilesAspect.addRuntime(DevFilesMain);
